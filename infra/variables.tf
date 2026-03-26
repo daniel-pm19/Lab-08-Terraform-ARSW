01e266547cfa54fd4fb64c67b61d637f7753d4f4
DanielPatiñoMejia <daniel.patino-m@mail.escuelaing.edu.co>
2026-03-25 23:08:31 -0500

    add(core): backend remote bootstrap completed, main variables filled and VMs cloud init

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,3 +5,17 @@ variable "admin_username" { type = string }
 variable "ssh_public_key" { type = string }
 variable "allow_ssh_from_cidr" { type = string }
 variable "tags" { type = map(string) }
+
+
+prefix = "lab8"
+location = "eastus"
+vm_count = 2
+admin_username = "danielPatinoAzure"
+ssh_public_key = "~/.ssh/id_ed25519.pub"
+allow_ssh_from_cidr = "192.168.2.3/32"
+tags = {
+    owner = "danielpm",
+    course = "ARSW",
+    env = "dev",
+    expieres = "2026-12-31"
+}
